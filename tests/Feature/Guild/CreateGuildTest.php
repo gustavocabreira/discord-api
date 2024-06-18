17afce8e522f7180f5e8a3bb10b5af55599542fe
Gustavo Cabreira <gustavo.softdev@gmail.com>
2024-06-18 10:39:01 -0300

    test: user must be authenticated to create a guild

--- tests/Feature/Guild/CreateGuildTest.php
+++ tests/Feature/Guild/CreateGuildTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Guild;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class CreateGuildTest extends TestCase
@@ -19,10 +20,25 @@ class CreateGuildTest extends TestCase
         ];
 
         $response = $this->actingAs($user)->postJson(route('api.guilds.store'), $payload);
+
         $response
             ->assertStatus(201)
             ->assertJsonStructure(['id', 'name']);
 
         $this->assertDatabaseHas('guilds', $payload);
+    }
+
+    public function test_user_must_be_authenticated_to_create_a_guild(): void
+    {
+        $payload = [
+            'name' => fake()->name,
+        ];
+
+        $response = $this->postJson(route('api.guilds.store'), $payload);
+
+        $response
+            ->assertStatus(Response::HTTP_UNAUTHORIZED)
+            ->assertJsonStructure(['message'])
+            ->assertExactJson(['message' => 'Unauthenticated.']);
     }
 }
